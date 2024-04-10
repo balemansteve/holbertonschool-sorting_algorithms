4a9486b056cf7bdd8252200ef92ce0f717f3af83
root <manuelantognazza@gmail.com>
2024-04-09 21:32:55 -0300

    insertion sort list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+*insertion_sort_list -  Insertion sort algorithm
+*@list: lista a ordenar
+*
+*Return: void;
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *aux, *node, *pos;
+
+	if (!list || !*list)
+		return;
+
+	aux = *list;
+
+	while (aux)
+	{
+		node = aux;
+		while ((node->n < node->prev->n) && node->prev)
+		{
+			pos = node->prev;
+
+			if (node->next)
+				node->next->prev = pos;
+
+			if (pos->prev)
+				pos->prev->next = node;
+
+			else
+				*list = node;
+
+			pos->next = node->next;
+			node->prev = pos->prev;
+			node->next = pos;
+			pos->prev = node;
+
+			print_list(*list);
+		}
+
+		aux = aux->next;
+	}
+}
